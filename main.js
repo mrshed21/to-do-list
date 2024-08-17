0b16e11a1dc9cb4e3616f691ca67f87d1cccd1e1
unknown <zhm.othman@gmail.com>
2024-08-17 11:15:05 +0200

    fixing grid

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const addTask = document.getElementById("add-btn");
 const outPut = document.getElementById("output-sec");
 const deleteAll = document.getElementById("delete-all");
 
-
 let arr = [];
 if (localStorage.dataLocal != null) {
   arr = JSON.parse(localStorage.getItem("dataLocal"));
@@ -46,28 +45,30 @@ function showData() {
   let dataTask = "";
   for (let i = 0; i < arr.length; i++) {
     dataTask += `
-            <div class="task row border border-5 border-light bg-primary mb-5">
-                <h2 class="text-white  col-7 mx-5 fs-1 fw-bold py-4" id="title">${arr[i].containtTask}</h2>
-                <div class="col-4 py-4">
-                    <button onclick="editTask (${i})" class="btn btn-success me-5 fs-2 px-3" id="update">update</butto>
-                    <button onclick="deleteTask (${i})" class="btn btn-danger fs-2 px-3" id="delete">delete</button>
+            <div class="task row border border-5 border-light bg-primary my-2 ">
+                <h2 class="text-white  col-lg-7 col-md-5 mx-5 fs-3 fw-bold py-4" id="title">${arr[i].containtTask}</h2>
+                <div class="col-lg-4 col-md-5 py-4 d-flex justify-content-around w-25">
+                    <button onclick="editTask (${i})" class="btn btn-success me-3 fs-5 px-3" id="update">update</butto>
+                    <button onclick="deleteTask (${i})" class="btn btn-danger fs-5 px-3" id="delete">delete</button>
                 </div>
             </div>
         `;
   }
 
-if (arr == ""){
-    deleteAll.innerHTML = ""    
-} else {
+  if (arr == "") {
+    deleteAll.innerHTML = "";
+  } else {
     deleteAll.innerHTML = `
             <button onclick="deleteAllTasks ()" class="btn btn-danger w-50 fs-1 ">Delete All Tasks (${arr.length})</button>
 
-    `
-}
+    `;
+  }
 
   task.value = "";
   outPut.innerHTML = dataTask;
 }
+
+
 
 function deleteTask(i) {
   arr.splice(i, 1);
@@ -83,7 +84,6 @@ function editTask(i) {
   iValue = i;
 }
 
-
 /* <div class="task">
 <h2 id="title">${arr[i].containtTask}</h2>
 <div>
@@ -93,13 +93,10 @@ function editTask(i) {
     </div>
 */
 
-function deleteAllTasks (){
-    arr =[];
-    localStorage.clear();
-    showData();
-    
-    
+function deleteAllTasks() {
+  arr = [];
+  localStorage.clear();
+  showData();
 }
-
 
 showData();
